57dd6e090afd0e471fd3b86f3df687dd90777a33
Ronak vaya <ronakvaya@gmail.com>
2025-04-29 10:12:53 +0530

    updated boilerplate with componenets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
 export default function Home() {
     return (
       <div className="flex items-center justify-center min-h-screen bg-blue-100">
         <h1 className="text-4xl font-bold text-blue-600">
           Hello World — ProwCRM 🚀
         </h1>
+        <Button>Click Me</Button>
       </div>
     )
   }
