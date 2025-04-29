aa9aa660c4772f036cf59eafc4af509020c0c9bb
Ronak vaya <ronakvaya@gmail.com>
2025-04-29 09:08:51 +0530

    boilerplate with tailwind working

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-blue-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello World — ProwCRM 🚀
+        </h1>
+      </div>
+    )
+  }
+  
